b3e2b071e3c76c291441bf98a5dbb596fae88757
jason <813000816@qq.com>
2017-07-17 12:16:49 +0800

    su

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wuwenhao on 2017/6/28.
+ */
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+    // 404 page
+    app.use(function (req, res) {
+        if (!res.headersSent) {
+            res.status(404).render('404');
+        }
+    });
+};
